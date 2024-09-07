374816874f589cd7b106388cf52df32c1140c62d
Abhinaya135 <abhinaya094@gmail.com>
2024-09-07 12:10:11 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class Student {
+    private int id;
+    private String name;
+    private String course;
+
+    public Student(int id, String name, String course) {
+        (link unavailable) = id;
+        this.name = name;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+}
